8c6732f3c510dc3b8b1a3db7c08b04b00a932764
Delio Lustosa Cantarelli <dlc@cin.ufpe.br>
2015-12-13 16:10:28 -0300

    comments ready but not tested

--- app/src/main/java/br/ufpe/cin/vocalium/StudentListenComment.java
+++ app/src/main/java/br/ufpe/cin/vocalium/StudentListenComment.java
@@ -7,6 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Vector;
+
+import AudioUtils.AudioComment;
+import AudioUtils.AudioCommentPlayer;
+import Utils.LayoutOutput;
+
 public class StudentListenComment extends AppCompatActivity {
 
     @Override
@@ -24,6 +32,37 @@ public class StudentListenComment extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        Vector<AudioComment> comments = CreateComments();
+        try {
+            AudioCommentPlayer player = new AudioCommentPlayer(getApplicationContext(), 0, comments, this);
+        } catch (IOException e) {
+            LayoutOutput.getInstance().ChangeStudentCommentText("Cannot open media", this);
+        }
     }
 
+    private Vector<AudioComment> CreateComments ()
+    {
+        Calendar time1 = Calendar.getInstance();
+        time1.setTimeInMillis(10000);
+        Calendar time2 = Calendar.getInstance();
+        time1.setTimeInMillis(20000);
+        Calendar time3 = Calendar.getInstance();
+        time1.setTimeInMillis(30000);
+
+        String text1 = "Muito bom aqui";
+        String text2 = "está aprendendo";
+        String text3 = "Parabéns";
+
+
+        AudioComment comment1 = new AudioComment(time1, text1);
+        AudioComment comment2 = new AudioComment(time2, text2);
+        AudioComment comment3 = new AudioComment(time3, text3);
+
+        Vector<AudioComment> commentVector = new Vector<>();
+        commentVector.add(comment1);
+        commentVector.add(comment2);
+        commentVector.add(comment3);
+
+        return commentVector;
+    }
 }
